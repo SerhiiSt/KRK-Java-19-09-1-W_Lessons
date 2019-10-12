cd51e667ae497d5dbf0f25c872055e985100f019
Sergio <shtelmah.serhiy@gmail.com>
2019-10-12 17:56:58 +0200

    reworked code

--- Lesson2/Task5_Lesson2.java
+++ Lesson2/Task5_Lesson2.java
@@ -1,13 +1,13 @@
  public class Task5_Lesson2{
             public static void main(String ... args){
             
-                   int count =0;
-                   int argsIndex = args.length-1;
-                   if(args.length==0) System.out.println("No arguments");
-                   while(count<args.length){
-                         System.out.println(args[argsIndex]);
-                         argsIndex--;
-                         count++;
+        int index = args.length - 1;
+        if (args.length == 0) System.out.println("No arguments");
+        while (index >= 0) {
+            System.out.println(args[index]);
+            index--;
+
+                         
                    }
                    
             }

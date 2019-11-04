60dc96116961d1d11e94023442b76b47ae98d79a
Sergio <shtelmah.serhiy@gmail.com>
2019-11-04 23:38:49 +0100

    added  to lesson 6  Algorithm fo find longest and shortest words

---
+++ src/pl/itacademy/lesson6/Words.java
@@ -0,0 +1,43 @@
+package pl.itacademy.lesson6;
+
+
+public class Words {
+
+    public static void checkTheLengthOfWords(String words) {
+
+        String[] text = words.split(" ");
+        String longWord = text[0];
+        String shortestWord = text[0];
+        String listOfLongestWords = "";
+        String listOfShortestWords = "";
+
+        for (int i = 0; i < text.length; i++) {
+
+            if (text[i].length() > longWord.length()) {
+                listOfLongestWords = listOfLongestWords + " " + text[i];
+                longWord = text[i];
+
+            }
+        }
+
+        for (int i = 0; i < text.length; i++) {
+            if (text[i].length() <= shortestWord.length()) {
+                listOfShortestWords = listOfShortestWords + " " + text[i];
+                shortestWord = text[i];
+
+            }
+        }
+
+
+        System.out.println("List of longest words is " + "[" + listOfLongestWords + "]");
+        System.out.println("List of shortest words is " + "[" + listOfShortestWords + "]");
+
+    }
+
+    public static void main(String[] args) {
+        String text = "For given text find the longest and the shortest words";
+        checkTheLengthOfWords(text);
+
+    }
+
+}

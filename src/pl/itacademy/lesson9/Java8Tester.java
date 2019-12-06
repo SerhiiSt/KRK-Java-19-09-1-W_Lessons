faa31e2cf7e6a6e0d40b3292d4493ddd501d9051
Sergio <shtelmah.serhiy@gmail.com>
2019-12-06 21:46:27 +0100

    added  stream implementation

--- src/pl/itacademy/lesson9/Java8Tester.java
+++ src/pl/itacademy/lesson9/Java8Tester.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
-
 public class Java8Tester {
     public static void main(String[] args) {
         Address toronto = new Address("Toronto");
